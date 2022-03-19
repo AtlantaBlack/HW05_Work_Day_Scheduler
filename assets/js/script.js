9654cd59330b7e5c81d9e175e07026dd19c3f581
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-03-19 20:28:49 +1100

    trying to add unique ID to each hour block row

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,50 +15,56 @@ setInterval(function() {
 
 // TIME BLOCKS
 
-const dayStart = 9; // 9 am 
-const dayEnd = 17; // 5 pm
 
-const workDay = [
+ // hours for work day
+ const workDay = [
     9, 10, 11, 12, 13, 14, 15, 16, 17
 ]
 
-populatePlanner();
+loadPlanner();
 
-function populatePlanner() {
+// put the time blocks on the page
+function loadPlanner() {
+
+    // grab container to hold all the hours
     var planner = $(".container");
-    var currentTime = moment().hour();
+    // grab current hour using moment
+    var currentHour = moment().hour();
 
+    // jquery for each, use the workday array and its index + index value
     jQuery.each(workDay, function(i, value) {
         console.log([i] + " " + value);
 
         var hourArticle = $("<article>");
         hourArticle.addClass("row border-red");
+        hourArticle.attr("id", "newID"); // <---- ?????
         planner.append(hourArticle)
 
-        var hourList;
+        var hourBlock;
         var textInput;
         var saveButton;
         var icon;
 
-        hourList = $("<section>");
-        hourList.addClass("col-2 border-green");
-        hourList.text(moment(value, "H").format("h A"));
-        hourArticle.append(hourList);
+        hourBlock = $("<section>");
+        hourBlock.addClass("col-2 border-green");
+        hourBlock.text(moment(value, "H").format("h A"));
+        hourArticle.append(hourBlock);
 
         textInput = $("<textarea>");
         textInput.addClass("col-8 border-green");
         hourArticle.append(textInput);
 
-        if (value < currentTime) {
+        if (value < currentHour) {
             textInput.addClass("past");
-        } else if (value === currentTime) {
+        } else if (value === currentHour) {
             textInput.addClass("present");
         } else {
             textInput.addClass("future");
         }
 
         saveButton = $("<button>");
-        saveButton.addClass("col-2 border-green");
+        saveButton.addClass("col-2 saveBtn border-green");
+        saveButton.on("click", saveAndUpdate);
 
         icon = $("<i>");
         icon.addClass("fas fa-save");
@@ -70,6 +76,19 @@ function populatePlanner() {
 }
 
 
+function saveAndUpdate(event) {
+    event.preventDefault();
+    
+    // set item to storage
+    // update item to storage
+    // get item from storage
+}
+
+function saveToStorage() {
+    var workDaySchedule = {
+        
+    }
+}
 
 
 
